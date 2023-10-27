8222a01d035ca32740309eff7ba67fba08be09d4
Arun kumar <arun.kumar@icanbwell.com>
2023-10-27 00:23:10 -0700

    Changes

--- app/src/main/java/com/task/ibell/activities/ui/home/HomeFragment.kt
+++ app/src/main/java/com/task/ibell/activities/ui/home/HomeFragment.kt
@@ -6,14 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.task.ibell.BWellSampleApplication
+import com.task.ibell.R
 import com.task.ibell.data.model.ActivityListItems
 import com.task.ibell.databinding.FragmentHomeBinding
 import com.task.ibell.viewmodel.SharedViewModelFactory
 import com.task.ibell.viewmodel.SharedViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -32,6 +34,10 @@ class HomeFragment : Fragment() {
         mainViewModel.suggestedActivities.observe(viewLifecycleOwner) {
             setAdapter(it.suggestedActivitiesLIst)
         }
+
+        binding.seeMore.setOnClickListener(this)
+        binding.homeView.btnGetStarted.setOnClickListener(this)
+
         return root
     }
 
@@ -41,8 +47,16 @@ class HomeFragment : Fragment() {
         binding.rvSuggestedActivities.adapter = adapter
     }
 
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.see_more -> findNavController().navigate(R.id.nav_health_journey)
+            R.id.btn_get_started -> findNavController().navigate(R.id.nav_data_connections)
+        }
     }
 }
